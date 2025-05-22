36f6a0a7cece7449469b499ffc9b3125cf2aef9f
manimozhidharmaraj <manimozhidharmaraj255@gmail.com>
2025-05-22 00:50:13 -0400

    added files

--- src/main/java/com/example/Handler.java
+++ src/main/java/com/example/Handler.java
@@ -1,11 +1,12 @@
+
 package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Object, String> {
+public class Handler implements RequestHandler<String, String> {
     @Override
-    public String handleRequest(Object input, Context context) {
-        return "Hello from Lambda!";
+    public String handleRequest(String input, Context context) {
+        return "Hello, " + input;
     }
 }
